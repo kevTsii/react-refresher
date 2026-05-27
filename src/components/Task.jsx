4a0ed65e45a447815aa9d0f2a50edd109f3f7d7b
Tsiory RAKOTOSOA <t.rakotosoa@artamis.tech>
2026-05-27 22:02:55 +0300

    feat(crud-j1): Claude Code enhancing css

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -24,12 +24,29 @@ export function Task({task, toggleTask, removeTask, updateTask}) {
                 />
                 {!editing
                     ? <span className="todo-item-name">{task.name}</span>
-                    : <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} onKeyDown={handleKeyDown}/>
+                    : <input
+                        className="todo-edit-input"
+                        type="text"
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                        onKeyDown={handleKeyDown}
+                        autoFocus
+                      />
                 }
 
-                <button onClick={() => setEditing(true)}>Edit</button>
-                <button className="btn-remove" onClick={() => removeTask(task.id)}>
-                    Supprimer
+                <button className="btn-icon btn-edit" title="Modifier" onClick={() => setEditing(true)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+                    </svg>
+                </button>
+                <button className="btn-icon btn-remove" title="Supprimer" onClick={() => removeTask(task.id)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <polyline points="3 6 5 6 21 6"/>
+                        <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/>
+                        <path d="M10 11v6M14 11v6"/>
+                        <path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"/>
+                    </svg>
                 </button>
             </li>
         </>
